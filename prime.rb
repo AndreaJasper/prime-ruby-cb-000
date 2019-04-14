2ffd1d7a61e809bd1e82bb280d4b3b0444e3be62
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-14 22:41:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 # Add  code here!
 def prime?(num)
-  for num in 2..(num - 1)
-    if (num % d ) == 0
-      return false
-    end
+  n = 1
+  while n < num
+    return false if num % n == 0
+    n += 1
   end
   true
 end
